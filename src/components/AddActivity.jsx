5103f63065ab98d19fd7efec37c3408e9042c8f7
Faiz Alkautsar <alkafaiz99@gmail.com>
2021-10-19 03:20:00 +0800

    adjust for automation testing

--- src/components/AddActivity.jsx
+++ src/components/AddActivity.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRefreshActivity } from '../helper/ActivityContext';
 import { createActivity } from '../services/appService';
 import AddIcon from './AddIcon';
@@ -7,21 +7,25 @@ import toast from 'react-hot-toast';
 
 function AddActivity() {
     const refreshActivity = useRefreshActivity();
+    const [isLoading, setIsLoading] = useState(false);
 
     const onSubmit = async () => {
         try {
+            setIsLoading(true);
             await createActivity('New Activity');
             refreshActivity();
+            setIsLoading(false);
             toast.success('Activity berhasil ditambahkan');
         } catch (error) {
             console.log(error);
+            setIsLoading(false);
             toast.error('Activity gagal ditambahkan');
         }
     };
 
     return (
         <div>
-            <Button data-cy="activity-add-button" onClick={onSubmit} startIcon={<AddIcon />}>
+            <Button disabled={isLoading} data-cy="activity-add-button" onClick={onSubmit} startIcon={<AddIcon />}>
                 Tambah
             </Button>
         </div>
