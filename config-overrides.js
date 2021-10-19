c26bc5468cbc7abeef314907d5a33221a727d47c
Faiz Alkautsar <alkafaiz99@gmail.com>
2021-10-19 15:30:04 +0800

    configure preact

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const { override, addWebpackAlias, addPostcssPlugins, addExternalBabelPlugins } = require('customize-cra');
+
+module.exports = override(
+    ...addExternalBabelPlugins([
+        '@babel/plugin-transform-react-jsx',
+        {
+            pragma: 'h',
+            pragmaFrag: 'Fragment',
+        },
+    ]),
+    addPostcssPlugins([require('tailwindcss'), require('autoprefixer')]),
+    addWebpackAlias({
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat', // Must be below test-utils
+        'react/jsx-runtime': 'preact/jsx-runtime',
+    })
+);
